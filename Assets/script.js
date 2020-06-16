bbe87eba3112bc35012f6806d9503a67fd947b8b
karleighponnock <karleighponnock@gmail.com>
2020-06-16 17:14:17 -0400

    psuedo-code created for elements of page

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+// create questions as objects\
+// hook container element
+
+// hook timer element
+
+// create dynamic H1 tag
+
+// create dynamic start button
+
+// create dynamic p tag to display and switch between questions
+
+// declare global variables
+
+
