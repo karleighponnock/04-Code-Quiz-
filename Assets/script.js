860a77f9c9aa929ca9d5ee9ea23bba933fb99368
karleighponnock <karleighponnock@gmail.com>
2020-06-16 17:21:36 -0400

    psuedo-code for what when and how the elements will function dynamically

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
 // create questions as objects\
+
 // hook container element
 
 // hook timer element
@@ -12,3 +13,20 @@
 // declare global variables
 
 
+//Display Initial page
+
+//Click start button
+
+//Start timer with interval of 1 second
+
+//Display Question 1
+
+//if correct display correct + go to next question
+
+
+//if incorrect display incorrect + subtract 2 seconds from timer + go to next
+
+
+//Stop Quiz if timer runs out or all questions are complete
+
+//display results
